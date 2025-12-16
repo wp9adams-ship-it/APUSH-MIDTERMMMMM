041585f3935ac044ecccf30543f0901c66f06f25
Crystallity <149009337+Crystallity@users.noreply.github.com>
2025-12-16 17:35:16 -0500

    Add utf-8 support to scripts
    An apostrophe in Characteristics of Washingtons presidency.html was breaking the code.

--- script/include_article-theme-js.py
+++ script/include_article-theme-js.py
@@ -7,12 +7,12 @@ chdir('APUSH study')
 for file in listdir():
     split = file.split('.')
     if split[len(split) - 1] == 'html':
-        with open(file, 'r') as content:
+        with open(file, 'r', encoding='utf-8') as content:
             text = content.read()
             if 'article-theme-js' not in text:
                 modified = text.replace('</title>', f'</title>\n    <script src="article-theme-js/theme.js" defer></script>')
                 print(f'Added script to "{file}"')
             else:
                 modified = text
-        with open(file, 'w') as f:
+        with open(file, 'w', encoding='utf-8') as f:
             f.write(modified)
--- script/set_title_to_document.py
+++ script/set_title_to_document.py
@@ -7,12 +7,12 @@ chdir('APUSH study')
 for file in listdir():
     split = file.split('.')
     if split[len(split) - 1] == 'html':
-        with open(file, 'r') as content:
+        with open(file, 'r', encoding='utf-8') as content:
             text = content.read()
             if '<title>Document</title>' in text:
                 modified = text.replace('<title>Document</title>', f'<title>{split[0]}</title>')
                 print(f'Updated title in "{file}" to "{split[0]}"')
             else:
                 modified = text
-        with open(file, 'w') as f:
+        with open(file, 'w', encoding='utf-8') as f:
             f.write(modified)
